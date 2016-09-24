3d0e3fda90919d3a9069ea47f8eefd00af504287
starcolon <tao@starcolon.com>
2016-09-25 01:24:47 +0700

    Initial CNN structure with #theanets

--- model/cnn.py
+++ model/cnn.py
@@ -7,13 +7,43 @@ Convolutional Neural Network
 from theano import *
 from theano import tensor as T
 from scipy import *
+import theanets as N
+import theanets.layers.convolution as C
 import numpy as np
 from . import *
 
-# Create an empty structure of a CNN model
-def init_models(num_target_vals):
-  X = T.fmatrix('X')
-  Y = T.fvector('Y')
-  pass
+class CNN():
+
+  """
+  @param {(Int,Int)} dimension of images
+  """
+  def __init__(self, image_dim, final_vec_dim)
+
+    w,h = image_dim
+
+    l1 = C.Conv1(w*h)
+    l2 = C.Conv1(round(l1/2))
+    l3 = C.Conv1(l2-2)
+    l4 = (round(l1/2), 'sigmoid')
+    l5 = (final_vec_dim, 'linear')
+
+    # Create a NN structure
+    self.net = N.regressor(\
+      layers=[l1, l2, l3, l4, l5]\
+      )
+
+    # Adjust parameters
+    self.add_loss('mae', weight=0.1)
+
+
+  def train(self,trainset,validationset):
+    self.net.train(\
+      trainset, validationset,\
+      algo='rmsprop',\
+      hidden_l1=0.01 )
+
+  def predict(self,candidate):
+    return self.net.predict(candidate)
+
 
 
--- model/fundamental.py
+++ model/fundamental.py
@@ -9,16 +9,18 @@ from theano import tensor as T
 from scipy import *
 import numpy as np
 
-class HiddenNode():
+class HiddenNodes():
   """
   @param {Int} dimensionality of input matrix
   @param {Int} dimensionality of output matrix (number of next hidden units)
   @param {T.Opr} activation function
   @param {np.array 2dim} initial node weight: (#dimX, #dimY)
+  @param {np.array} initial b vector
   """
-  def __init__(self, X_dim, Y_dim, activation=T.tanh, W=None):
+  def __init__(self, X_dim, Y_dim, activation=T.tanh, W=None, b=None):
     self.activation = activation
     self.W = W if W is not None else __init_weight(seed, X_dim, Y_dim)
+    self.b = b if b is not None else __init_b(Y_dim)
 
 
   def __init_weight(self, X_dim, Y_dim):
@@ -27,9 +29,18 @@ class HiddenNode():
       -np.sqrt(6.0/(X_dim+Y_dim)),\
       np.sqrt(6.0/(X_dim+Y_dim)),\
       (X_dim, Y_dim)),\
-    dtype=theano.config.floatX)
+    dtype=config.floatX)
+
+    # For sigmoid activation, use 4 times higher weight
+    if self.activation == T.nnet.sigmoid:
+      W_vec *= 4.0
 
     return shared(value=W_vec, name='W', borrow=True)
+
+
+  def __init_b(self, Y_dim):
+    b_vec = np.zeros((Y_dim,), dtype=config.floatX)
+    return shared(value=b_vec, name='b', borrow=True)
 
 
   """
@@ -40,4 +51,7 @@ class HiddenNode():
     out = T.dot(X, self.W) + self.b
     if self.activation is None: out
     else: self.activation(out)
+
+
+
 
