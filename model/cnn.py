83c6fdda4ff0a75c007d5bcdb59849a8005fe172
starcolon <tao@starcolon.com>
2016-09-26 23:57:32 +0700

    CNN : Rectify layers structure

--- model/cnn.py
+++ model/cnn.py
@@ -23,23 +23,11 @@ class CNN():
   """
   def __init__(self, image_dim, final_vec_dim):
 
-    n1 = round(image_dim/16)
-    n2 = round(n1/8)
-    n3 = final_vec_dim
-
-    print('...CNN layers nodes:')
-    print('...input dim: {0}'.format(image_dim))
-    print('...layer #1 : {0} nodes'.format(n1))
-    print('...layer #2 : {0} nodes'.format(n2))
-    print('...layer #3 : {0} nodes'.format(n3))
-
     l1 = ('input',   layers.InputLayer)
-    l2 = ('conv2d1', layers.Conv2DLayer)
+    l2 = ('conv1',   layers.Conv2DLayer)
     l3 = ('pool1',   layers.MaxPool2DLayer)
     l4 = ('hidden1', layers.DenseLayer)
     l5 = ('output',  layers.DenseLayer)
-
-
 
     # Create a NN structure
     print('...Building initial structure')
@@ -47,7 +35,7 @@ class CNN():
       layers=[l1, l2, l3, l4, l5],
       input_shape=(None, 1, image_dim, image_dim*3),
       conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-      hidden4_num_units=400,
+      hidden1_num_units=400,
       output_num_units=final_vec_dim,
       update_learning_rate=0.1,
       update_momentum=0.8,
