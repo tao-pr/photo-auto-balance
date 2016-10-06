932349b399afe3d17eea386d8c4bed9a707ff6f2
starcolon <tao@starcolon.com>
2016-10-06 22:43:37 +0700

    CNN : Adjust layer params

--- model/cnn.py
+++ model/cnn.py
@@ -30,20 +30,22 @@ class CNN():
     l4 = ('conv2',   layers.Conv2DLayer)
     l5 = ('pool2',   layers.MaxPool2DLayer)
     l6 = ('hidden1', layers.DenseLayer)
-    l7 = ('output',  layers.DenseLayer)
+    l7 = ('hidden2', layers.DenseLayer)
+    l8 = ('output',  layers.DenseLayer)
 
     # Create a NN structure
     print('...Building initial structure')
     print('...input size of  : ', image_dim)
     print('...output size of : ', final_vec_dim)
     self.net = NeuralNet(
-      layers=[l1, l2, l3, l4, l5, l6, l7],
+      layers=[l1, l2, l3, l4, l5, l6, l7, l8],
       input_shape=(None,) + image_dim,
-      conv1_num_filters=100, conv1_filter_size=(5, 5), 
+      conv1_num_filters=50, conv1_filter_size=(5, 5), 
       pool1_pool_size=(3, 3),
-      conv2_num_filters=100, conv2_filter_size=(7, 7),
+      conv2_num_filters=25, conv2_filter_size=(7, 7),
       pool2_pool_size=(3, 3),
       hidden1_num_units=128,
+      hidden2_num_units=16,
       output_num_units=final_vec_dim, output_nonlinearity=None,
       update_learning_rate=0.001,
       update_momentum=0.8,
